3ef9a7cbb25e5c9e6a5c38240f56ab0c8cd9fffb
A-xuan-xuan <156889137+A-xuan-xuan@users.noreply.github.com>
2024-01-20 14:42:09 +0800

    Add files via upload

---
+++ CODE/文件学习.py
@@ -0,0 +1,122 @@
+import time
+
+#
+# f = open("E:\Python学习\文件学习.txt","r", encoding = "UTF-8")
+# print(type(f))
+#
+# # print(f.read(10))
+# # print(f.read())
+# # print(f.read())
+# print("------------------------------")
+#
+# # print(f.readline())
+# # print(f.readlines())
+# #
+# for lin in f:
+#     print(lin)
+#
+# f.close()
+
+
+# with open("E:\Python学习\文件学习.txt","r", encoding = "UTF-8") as f:
+#     for lin in f:
+#         print(lin)
+
+#
+# f = open("E:\Python学习\文件学习2.txt","w",encoding="UTF-8")
+# f.write("Hello World!!!")
+# #f.flush()
+# f.close()
+# f = open("E:\Python学习\文件学习2.txt","w",encoding="UTF-8")
+# f.write("复活吧！我的爱人！")
+# #f.flush()
+# f.close()
+
+
+# f = open("E:\Python学习\文件学习3.txt","a",encoding="UTF-8")
+# f.write("无极帝国长公主顾清寒！！！\n")
+# f.flush()
+# f.close()
+
+
+# fr=open("E:\Python学习\文件学习4.txt","r",encoding="UTF-8")
+# fw=open("E:\Python学习\文件学习4.txt.bak","w",encoding="UTF-8")
+# for line in fr:
+#     line = line.strip()
+#     print(type(line))
+#     if line.split(",")[-1] == "测试":
+#         continue
+#     fw.write(line)
+#     fw.write("\n")
+#
+# fr.close()
+# fw.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
